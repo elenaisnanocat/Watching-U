e3c68530a65567f2625a076e0484ac198235c9f8
elenaisnanocat <elenassafy0201@gmail.com>
2021-11-21 00:41:33 +0900

    nav carousel update login sign up logout ok

--- watchingu/settings.py
+++ watchingu/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'accounts',
     'movies',
     'community',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
