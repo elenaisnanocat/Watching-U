0206ccbe8529e56330111dd98fe85990525d7365
yoonhyeyoon <hyeyun3306@gmail.com>
2021-11-24 13:16:46 +0900

    wordcloud crawling

--- naver_api.py
+++ naver_api.py
@@ -22,7 +22,7 @@ movies = Movie.objects.all()
 for movie in movies:
     title = movie.title
 
-    # # 드라이버 경로지정 => 해당 코드가 실행되면서 브라우저 열림
+    # 드라이버 경로지정 => 해당 코드가 실행되면서 브라우저 열림
     # driver = webdriver.Chrome("./chromedriver.exe")
 
     # url = 'https://movie.naver.com/'
@@ -66,33 +66,36 @@ for movie in movies:
 
     # ---------------------------------------------------------
     ## 워드클라우드
-    f = open('./files_crawling/' + title + '.txt', 'r', encoding='utf-8')
-    data = f.read()
+    try:
+        f = open('./files_crawling/' + title + '.txt', 'r', encoding='utf-8')
+        data = f.read()
 
-    if len(data) == 0: 
+        if len(data) == 0: 
+            continue
+            # pass
+        okt = Okt()
+        noun_data = okt.nouns(data)
+                
+        # 불용어 제거
+        stopword=[]
+        word=[i for i in noun_data if i not in stopword]
+        word=[i for i in word if len(i)>1]  
+
+        data_cnt=collections.Counter(noun_data) 
+
+        # font_name=font_manager.FontProperties(fname="C:/USERS/USER/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/A옛날사진관3.TTF"). get_name()
+        font_name=font_manager.FontProperties(fname="C:/Windows/Fonts/malgun.ttf"). get_name()
+        rc('font', family=font_name)
+
+        # wc=WordCloud(font_path="C:/USERS/USER/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/A옛날사진관3.TTF", width=900, height=500, colormap = 'RdPu', background_color='white', max_words=100, stopwords=STOPWORDS).generate_from_frequencies(data_cnt)
+        wc=WordCloud(font_path="C:/Windows/Fonts/malgun.ttf", width=600, height=900, colormap = 'Purples', background_color='white', max_words=100, stopwords=STOPWORDS).generate_from_frequencies(data_cnt)
+        # wordcloud.recolor(color_func = "colorbrewer.diverging.Spectral_11")
+        wc.to_file('static/'+ title +'.png')
+        plt.figure(figsize=(10,10))
+        plt.imshow(wc)
+        plt.axis('off')
+        plt.show(block=False)
+        plt.pause(0.3)
+        plt.close()
+    except OSError as e:
         continue
-        # pass
-    okt = Okt()
-    noun_data = okt.nouns(data)
-            
-    # 불용어 제거
-    stopword=[]
-    word=[i for i in noun_data if i not in stopword]
-    word=[i for i in word if len(i)>1]  
-
-    data_cnt=collections.Counter(noun_data) 
-
-    # font_name=font_manager.FontProperties(fname="C:/USERS/USER/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/A옛날사진관3.TTF"). get_name()
-    font_name=font_manager.FontProperties(fname="C:/Windows/Fonts/malgun.ttf"). get_name()
-    rc('font', family=font_name)
-
-    # wc=WordCloud(font_path="C:/USERS/USER/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/A옛날사진관3.TTF", width=900, height=500, colormap = 'RdPu', background_color='white', max_words=100, stopwords=STOPWORDS).generate_from_frequencies(data_cnt)
-    wc=WordCloud(font_path="C:/Windows/Fonts/malgun.ttf", width=600, height=900, colormap = 'Purples', background_color='white', max_words=100, stopwords=STOPWORDS).generate_from_frequencies(data_cnt)
-    # wordcloud.recolor(color_func = "colorbrewer.diverging.Spectral_11")
-    wc.to_file('static/'+ title +'.png')
-    plt.figure(figsize=(10,10))
-    plt.imshow(wc)
-    plt.axis('off')
-    plt.show(block=False)
-    plt.pause(0.3)
-    plt.close()
